20cef51cbfd13265ec202b10093067d661d0cd1a
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-13 02:17:38 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  count = 0
+  retArray = []
+  while count < array.length do
+    retArray.push(array[count]**2)
+    count += 1
+  end
+  return retArray
 end
